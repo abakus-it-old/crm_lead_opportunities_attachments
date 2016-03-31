36d53f023c5dea435aa7667953694ca45fc0de9b
Valentin Thirion <valentinthirion@gmail.com>
2016-03-31 17:35:15 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -6,9 +6,12 @@
     'website': "http://www.abakusitsolutions.eu",
     'category': 'CRM',
     'description': """
-    This modules adds the following to the CRM opportunity form:
-    - planned periodic value + period + yearly total
-    - 2 attachement fields for proposals and attachments
+CRM Leads improvements for AbAKUS
+This modules adds the following to the CRM opportunity form:
+- planned periodic value + period + yearly total
+- 2 attachement fields for proposals and attachments
+
+This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
     """,
     'data': ['crm_lead_view.xml',],
     'demo': [],
